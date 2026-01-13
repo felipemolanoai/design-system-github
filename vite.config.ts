90ede359dddbd25a85bc42950034b28db8165ac0
molano5858 <molano5858@gmail.com>
2026-01-13 14:06:17 -0500

    Configuracion de build para libreria

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ include: ['src'] }) // Genera los tipos automáticamente
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'), // El archivo de entrada que creamos antes
+      name: 'DesignSystemGithub',
+      fileName: 'design-system-github',
+    },
+    rollupOptions: {
+      // Nos aseguramos de NO meter React dentro del paquete
+      // (el proyecto que lo use ya tendrá React instalado)
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
