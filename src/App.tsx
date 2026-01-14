87d5c86fba3fed311fdea6dc541788f7e21e5f07
molano5858 <molano5858@gmail.com>
2026-01-14 14:37:00 -0500

    HCI Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from "./components/Button";
 function App() {
   return (
     <div>
-      <h1>Design System</h1>
-      <Button label="Click me" />
+      <h1>HCI Design System</h1>
+      <Button label="Button From Design System" />
     </div>
   )
 }
